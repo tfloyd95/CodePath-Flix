e9558319863de93eb487fbb97125dbe33171dcc8
Tavien <tavien@hm-webmaster.morgan.edu>
2018-02-04 07:15:26 -0800

    TableView

---
+++ Flixster/MoviesViewController.swift
@@ -0,0 +1,53 @@
+//
+//  MoviesViewController.swift
+//  Flixster
+//
+//  Created by Tavien on 2/2/18.
+//  Copyright © 2018 Warpedflo. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController,  UITableViewDataSource, UITableViewDelegate
+    
+    {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.delegate = self
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        cell.textLabel?.text = "row \(indexPath.row)"
+        print("row \(indexPath.row)")
+        return cell
+        
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Project 1 - *Name of App Here*
+# Project 1 - *Flixster  (clone)*
 
-**Name of your app** is a movies app using the [The Movie Database API](http://docs.themoviedb.apiary.io/#).
+**Flixster** is a movies app using the [The Movie Database API](http://docs.themoviedb.apiary.io/#).
 
 Time spent: **X** hours spent in total
 
