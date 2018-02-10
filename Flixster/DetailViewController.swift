cb9730498e7648ac1df8dcd1768401dc7dc4cb1f
Tavien <tavien@Taviens-iMac.local>
2018-02-10 10:22:10 -0800

    Added a DetailViewController

---
+++ Flixster/DetailViewController.swift
@@ -0,0 +1,69 @@
+//
+//  DetailViewController.swift
+//  Flixster
+//
+//  Created by Tavien on 2/10/18.
+//  Copyright © 2018 Warpedflo. All rights reserved.
+//
+
+import UIKit
+
+enum MovieKeys
+{
+    static let title = "title"
+    static let backdropPath = "backdrop_path"
+    static let posterPath = "poster_path"
+}
+
+class DetailViewController: UIViewController
+{
+    @IBOutlet weak var backDropImageView: UIImageView!
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    var  movie:[String: Any]?
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        if let movie = movie
+        {
+            titleLabel.text = movie[MovieKeys.title] as? String
+            releaseDateLabel.text = movie["release_date"] as? String
+            overviewLabel.text = movie["overview"] as? String
+            let backDropPathString = movie[MovieKeys.backdropPath] as! String
+            let posterPathString = movie[MovieKeys.posterPath] as! String
+            let baseUrlString = "https://image.tmdb.org/t/p/w500"
+            
+            let backdropURL = URL(string: baseUrlString + backDropPathString)!
+            backDropImageView.af_setImage(withURL: backdropURL)
+            
+            let posterpathURL = URL(string: baseUrlString + posterPathString)!
+            posterImageView.af_setImage(withURL: posterpathURL)
+            
+            
+        }
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Flixster/NowPlayingViewController.swift
+++ Flixster/NowPlayingViewController.swift
@@ -92,6 +92,17 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource, UITable
         cell.posterImageView.af_setImage(withURL: posterUrl)
         return cell
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        let cell = sender as! UITableViewCell
+        if let indexPath = tableView.indexPath(for: cell)
+        {
+            let movie = movies[indexPath.row]
+            let detailViewController = segue.destination as! DetailViewController
+            detailViewController.movie = movie
+            
+        }
+    }
     
     
     override func didReceiveMemoryWarning() {
